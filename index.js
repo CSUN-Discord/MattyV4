afa845a4d00be08573e9c51e66ebfe948b9e6d0e
fahadasat <fahad.asat.242@my.csun.edu>
2021-09-28 14:27:03 -0700

    Created command handler w/ file structure, resolve #4

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 // Import classes and files
-const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
+const fs = require("fs");
+const { Client, Collection, Intents } = require("discord.js");
 
 // Create a new discord client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+//create a collection to store all the commands
+client.commands = new Collection();
+
+//read all the js names in the /commands folder and insert them into an array
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+//go through the array of file names and insert them into the collection with the command name as the key
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  //search for the file using the command name as a key which is received by the user
+  client.commands.set(command.data.name, command);
+}
 
 //events
 client.once("ready", () => {
   //set the status
   client.user.setActivity("with discord.js documentation", { type: "PLAYING" });
   console.log(client.user.username);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  //retrieve the command details by using the command name as map key
+  const command = client.commands.get(interaction.commandName);
+
+  //if command not in the map then return
+  if (!command) return;
+
+  //execute the command or print error if it fails
+  try {
+    await command.execute(interaction);
+  } catch (e) {
+    console.error(e);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 //start the bot with the token from the config file
