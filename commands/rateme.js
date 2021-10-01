eded0fbcd6ea1c71cbeeaeefccd8986b096e87da
fahadasat <fahad.asat.242@my.csun.edu>
2021-10-01 16:30:03 -0700

    adds rateme command resolves #17

---
+++ commands/rateme.js
@@ -0,0 +1,23 @@
+/*
+This command will send a rating to the user
+*/
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+module.exports = {
+  ...new SlashCommandBuilder()
+    .setName("rateme")
+    .setDescription("Gives you a rating."),
+  permission: ["SEND_MESSAGES"],
+  cooldown: 5,
+
+  /**
+   *
+   * @param interaction
+   * @returns {Promise<void>}
+   */
+  async execute(interaction) {
+    await interaction.reply(
+      `**You are a ${Math.floor(Math.random() * 11).toString()}/10**`
+    );
+  },
+};
