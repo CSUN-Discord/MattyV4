7b09162adc8bc2d9377bffc828662d92c1b68181
fahadasat <fahad.asat.242@my.csun.edu>
2021-10-01 22:19:12 -0700

    creates prune command, resolves #29

---
+++ commands/prune.js
@@ -0,0 +1,82 @@
+/*
+This command will prune the messages of the channel or specific user
+*/
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  ...new SlashCommandBuilder()
+    .setName("prune")
+    .addNumberOption((option) =>
+      option
+        .setName("amount")
+        .setDescription(
+          "The amount of messages to delete from a channel or target."
+        )
+        .setRequired(true)
+    )
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The user to delete the messages of.")
+        .setRequired(false)
+    )
+    .setDescription("Prunes messages from the channel or a specific user."),
+  // name: "prune",
+  // description: "Prunes messages from the channel or a specific user.",
+  // options: [
+  //   {
+  //     name: "amount",
+  //     description: "The amount of messages to delete from a channel or target.",
+  //     required: true,
+  //     type: "NUMBER",
+  //   },
+  //   {
+  //     name: "user",
+  //     description: "The user to delete the messages of.",
+  //     required: false,
+  //     type: "USER",
+  //   },
+  // ],
+  permission: ["ADMINISTRATOR"],
+  cooldown: 5,
+
+  /**
+   *
+   * @param interaction
+   * @returns {Promise<void>}
+   */
+  async execute(interaction) {
+    const amount = interaction.options.getNumber("amount");
+    const user = interaction.options.getMember("user");
+
+    const messages = await interaction.channel.messages.fetch();
+
+    const responseEmbed = new MessageEmbed().setColor("DARK_RED");
+
+    if (user) {
+      let counter = 0;
+      const filtered = [];
+      (await messages).filter((m) => {
+        if (m.author.id === user.id && amount > counter) {
+          filtered.push(m);
+          counter++;
+        }
+      });
+      await interaction.channel.bulkDelete(filtered, true).then((messages) => {
+        responseEmbed.setDescription(
+          `🧹 Cleared ${messages.size} messages from ${user}.`
+        );
+        interaction.reply({ embeds: [responseEmbed] });
+      });
+    } else {
+      await interaction.channel.bulkDelete(amount, true).then((messages) => {
+        responseEmbed.setDescription(
+          `🧹 Cleared ${messages.size} messages from this channel.`
+        );
+        interaction.reply({ embeds: [responseEmbed] });
+      });
+    }
+  },
+};
