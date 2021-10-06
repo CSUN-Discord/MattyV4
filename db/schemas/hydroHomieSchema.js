8ac14fc88c22b9fe03bba6f70a26a9c4ddb83bb4
Fahad Asat <fahad.asat@tcs.com>
2021-10-06 00:39:55 -0700

    hydro homie mongoose schema

---
+++ db/schemas/hydroHomieSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+//database table for hydro homie
+
+const hydroHomieSchema = new Schema({
+  userId: String,
+  timer: Array,
+  waterDrank: Number,
+});
+
+module.exports = mongoose.model("hydroHomie", hydroHomieSchema);
