a6c90ce104c11bd5b57e03acf995e84cf39f2799
fahadasat <fahad.asat.242@my.csun.edu>
2021-10-07 11:53:54 -0700

    updated response to roommate search and marketplace

---
+++ events/messageCreate.js
@@ -0,0 +1,42 @@
+/*
+event that listens for message creation
+ */
+
+const Chat = require("easy-discord-chatbot");
+const chat = new Chat({ name: "Matty" });
+
+module.exports = {
+  name: "messageCreate",
+
+  /**
+   *
+   * @param message
+   * @returns {Promise<void>}
+   */
+  async execute(message) {
+    if (message.channel.id === "894103257335603270" && !message.author.bot) {
+      let reply = await chat.chat(message.content);
+      await message.reply(reply);
+    }
+
+    // if (message.channel.id === "523967992917393418" && !message.author.bot) {
+    //   message
+    //     .reply("Use the slash command **/marketplace**.")
+    //     .then((msg) => {
+    //       setTimeout(() => msg.delete(), 10000);
+    //       message.delete();
+    //     })
+    //     .catch();
+    // }
+    // //
+    // if (message.channel.id === "570809696135544835" && !message.author.bot) {
+    //   message
+    //     .reply("Use the slash command **/roommate**.")
+    //     .then((msg) => {
+    //       setTimeout(() => msg.delete(), 10000);
+    //       message.delete();
+    //     })
+    //     .catch();
+    // }
+  },
+};
