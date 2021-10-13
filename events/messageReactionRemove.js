4f64ea13e94e327b9309f253814d3491e8a520a8
Fahad Asat <fahad.asat@tcs.com>
2021-10-13 10:40:37 -0700

    created events to check for reactions, so it can work after bot restarts

---
+++ events/messageReactionRemove.js
@@ -0,0 +1,79 @@
+/*
+event that happens when someone removes a reaction to a message
+ */
+
+const {roleChangeChannelId} = require("../validation/channels.json");
+const {guildId} = require("../config.json");
+
+module.exports = {
+    name: "messageReactionRemove",
+    once: true,
+    /**
+     *
+     * @param reaction
+     * @param user
+     * @returns {Promise<void>}
+     */
+    async execute(reaction, user) {
+        if (reaction.message.partial) await reaction.message.fetch();
+        if (reaction.partial) await reaction.fetch();
+        if (user.bot) return;
+        if (!reaction.message.guild) return;
+
+        const guild = reaction.client.guilds.cache.get(guildId);
+
+        const resident = guild.roles.cache.find(
+            (r) => r.id === "491747936435306526"
+        );
+        const meetups = guild.roles.cache.find(
+            (r) => r.id === "601273275016019978"
+        );
+        const minecraft = guild.roles.cache.find(
+            (r) => r.id === "694775549196763177"
+        );
+        const koreaboo = guild.roles.cache.find(
+            (r) => r.id === "755254996995538995"
+        );
+        const lecture = guild.roles.cache.find(
+            (r) => r.id === "811796493622050826"
+        );
+        const templeFollower = guild.roles.cache.find(
+            (r) => r.id === "859649594815807489"
+        );
+        const movie = guild.roles.cache.find(
+            (r) => r.id === "726280155190001735"
+        );
+
+        if (reaction.message.channel.id === roleChangeChannelId) {
+            if (reaction.emoji.name === `1️⃣`) {
+                await reaction.message.guild.members.cache
+                    .get(user.id)
+                    .roles.remove(resident);
+            } else if (reaction.emoji.name === `2️⃣`) {
+                await reaction.message.guild.members.cache
+                    .get(user.id)
+                    .roles.remove(meetups);
+            } else if (reaction.emoji.name === `3️⃣`) {
+                await reaction.message.guild.members.cache
+                    .get(user.id)
+                    .roles.remove(minecraft);
+            } else if (reaction.emoji.name === `4️⃣`) {
+                await reaction.message.guild.members.cache
+                    .get(user.id)
+                    .roles.remove(koreaboo);
+            } else if (reaction.emoji.name === `5️⃣`) {
+                await reaction.message.guild.members.cache
+                    .get(user.id)
+                    .roles.remove(lecture);
+            } else if (reaction.emoji.name === `6️⃣`) {
+                await reaction.message.guild.members.cache
+                    .get(user.id)
+                    .roles.remove(templeFollower);
+            } else if (reaction.emoji.name === `7️⃣`) {
+                await reaction.message.guild.members.cache
+                    .get(user.id)
+                    .roles.remove(movie);
+            }
+        }
+    },
+};
