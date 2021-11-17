5fc6b04c48e1209ec02cd51816b79288a6267f22
Fahad Asat <fahad.asat.242@my.csun.edu>
2021-11-17 13:17:39 -0800

    minor changes, error handling for archiving thread, resolves #83

--- commands/archive-thread.js
+++ commands/archive-thread.js
@@ -3,29 +3,37 @@ This command will archive a thread
 */
 
 module.exports = {
-  name: "archive-thread",
-  description: "Used to delete a thread",
-  permission: ["SEND_MESSAGES"],
-  options: [
-    {
-      name: "thread-id",
-      description: "ID of the thread.",
-      required: true,
-      type: "STRING",
-    },
-  ],
+    name: "archive-thread",
+    description: "Used to delete a thread",
+    permission: ["SEND_MESSAGES"],
+    options: [
+        {
+            name: "thread-id",
+            description: "ID of the thread.",
+            required: true,
+            type: "STRING",
+        },
+    ],
 
-  /**
-   *
-   * @param interaction
-   * @returns {Promise<void>}
-   */
-  async execute(interaction) {
-    const id = interaction.options.getString("thread-id");
-    const thread = interaction.channel.threads.cache.find((x) => x.id === id);
-    if (thread) {
-      await thread.setArchived(true);
-      await interaction.reply("Thread archived.");
-    } else await interaction.reply("Thread not found.");
-  },
+    /**
+     *
+     * @param interaction
+     * @returns {Promise<void>}
+     */
+    async execute(interaction) {
+        try {
+            const id = interaction.options.getString("thread-id");
+            const thread = interaction.channel.threads.cache.find((x) => x.id === id);
+            if (thread) {
+                await thread.setArchived(true);
+                await interaction.reply({content: "Thread archived.", ephemeral: true});
+            } else await interaction.reply({content: "Thread not found.", ephemeral: true});
+        } catch (e) {
+            await interaction.reply({
+                content: "Make sure to use this command in a channel, not a thread and try again.",
+                ephemeral: true
+            });
+        }
+
+    },
 };
--- events/threadCreate.js
+++ events/threadCreate.js
@@ -6,34 +6,34 @@ const {auditChannelId} = require("../validation/channels.json");
 const {MessageEmbed} = require("discord.js");
 
 module.exports = {
-  name: "threadCreate",
-  once: false,
+    name: "threadCreate",
+    once: false,
 
-  /**
-   *
-   * @param thread
-   * @returns {Promise<void>}
-   */
+    /**
+     *
+     * @param thread
+     * @returns {Promise<void>}
+     */
 
-  async execute(thread) {
-    await thread.setLocked(true);
+    async execute(thread) {
+        await thread.setLocked(true);
 
-    const auditChannel = thread.client.channels.cache.get(auditChannelId);
+        const auditChannel = thread.client.channels.cache.get(auditChannelId);
 
-    try {
-      const user = await thread.client.users.fetch(thread.ownerId);
+        try {
+            const user = await thread.client.users.fetch(thread.ownerId);
 
-      const threadCreatEmbed = new MessageEmbed()
-          .setColor("RANDOM")
-          .setAuthor(`${thread.guild.name}`, `${thread.guild.iconURL()}`)
-          .setDescription(`Thread called ${thread.name} created in <#${thread.parentId}> by ${user} (${user.tag}).`)
-          .setFooter(`Creator: ${thread.ownerId} | Thread Id: ${thread.id}`)
-          .setTimestamp()
+            const threadCreatEmbed = new MessageEmbed()
+                .setColor("RANDOM")
+                .setAuthor(`${thread.guild.name}`, `${thread.guild.iconURL()}`)
+                .setDescription(`Thread called ${thread.name} created in <#${thread.parentId}> by ${user} (${user.tag}).`)
+                .setFooter(`Creator: ${thread.ownerId} | Thread Id: ${thread.id}`)
+                .setTimestamp()
 
-      return auditChannel.send({embeds : [threadCreatEmbed]})
-    } catch (e) {
-      console.log(e)
-      return auditChannel.send({content: `A thread was created, but there was a problem in retrieving data for it.`})
-    }
-  },
+            return auditChannel.send({embeds: [threadCreatEmbed]})
+        } catch (e) {
+            console.log(e)
+            return auditChannel.send({content: `A thread was created, but there was a problem in retrieving data for it.`})
+        }
+    },
 };
